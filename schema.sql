0f62bbd43fe5cc4e7f02a6ac88a2d2324dd90199
paul <paul@rundkorn.org>
2014-10-18 00:33:21 +0200

    add db schema

---
+++ schema.sql
@@ -0,0 +1,9 @@
+drop table if exists kekse;
+create table kekse (
+	id integer primary key autoincrement,
+	title text not null,
+	creator text not null,
+	description text not null,
+	latitude text not null,
+	longitude text not null
+);	
